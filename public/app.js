09dbf92c6e52285f91a6bca8d35d050287d867b9
AntonArk <anton.ark80@gmail.com>
2022-07-15 00:12:03 +0400

    Add Rakefile, update Gemfile, add Product model

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ function something()
 {
     var x = window.localStorage.getItem('bbb')
 
-    x = x * 1 + 1   // преобразовать строку в число
+    x = x * 1 + 1   // преобразовать строку в число x*1
 
     window.localStorage.setItem('bbb', x);
     
