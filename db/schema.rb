be2a5ec102d2e1076809656f1a5fe7d6e332d7de
AntonArk <anton.ark80@gmail.com>
2022-07-15 01:34:47 +0400

    Seed database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_14_201828) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_14_204806) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
