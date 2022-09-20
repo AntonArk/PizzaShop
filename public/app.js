4fc2d5453299f9746fa2e0936a4b45c4ef4ed099
AntonArk <anton.ark80@gmail.com>
2022-09-20 17:47:57 +0400

    Replace cart text if cart is empty

--- public/app.js
+++ public/app.js
@@ -73,7 +73,11 @@ function cart_get_orders()
 function cancel_order()
 {
   window.localStorage.clear();
+
   update_orders_input();
   update_orders_button();
+
+  $('#cart').text('Ваша корзина пуста.');
+
   return false;
 }
