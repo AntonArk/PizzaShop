b0a2b1ef8004659b89651b6adbf6611427644b86
AntonArk <anton.ark80@gmail.com>
2022-09-22 11:29:26 +0400

    Access to button via css

--- app.rb
+++ app.rb
@@ -12,10 +12,33 @@ end
 class Order < ActiveRecord::Base
 end
 
+def parse_orders_input orders_input
+
+	s1 = orders_input.split(/,/)
+
+	arr = []
+
+	s1.each do |x|
+  		s2 = x.split(/\=/)
+  	
+		s3 = s2[0].split(/_/)
+
+ 	 	id = s3[1]
+  	 	cnt = s2[1]
+
+		arr2 = [id,cnt]
+		
+		arr.push arr2
+	end
+
+        return arr
+end
+
 get '/' do
 	@products = Product.all
 	erb :index
 end
+
 
 get '/about' do
 	erb :about
@@ -37,38 +60,22 @@ post '/cart' do
 
 	if @items.length == 0
 		return erb :cart_is_empty
+	
+	elsif @items.each do |item|
+		  # id,cnt
+		  item[0] = Product.find(item[0])
+		  end
+		  erb:cart
 	end
 
 	# выводим список продуктов в корзине
 
-	@items.each do |item|
+	# @items.each do |item|
 		# id,cnt
-		item[0] = Product.find(item[0])
-	end
+	#	item[0] = Product.find(item[0])
+	# end
 
 	# возвращаем представление по умолчанию
 
-	erb :cart
-end
-
-def parse_orders_input orders_input
-
-	s1 = orders_input.split(/,/)
-
-	arr = []
-
-	s1.each do |x|
-  		s2 = x.split(/\=/)
-  	
-		s3 = s2[0].split(/_/)
-
- 	 	id = s3[1]
-  	 	cnt = s2[1]
-
-		arr2 = [id,cnt]
-		
-		arr.push arr2
-	end
-
-        return arr
+	#erb :cart
 end
